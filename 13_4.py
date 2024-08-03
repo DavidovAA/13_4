dfd6660610a331789df32e8fc8199248d45707d4
DavidovAA <aleksei-davydov8@mail.ru>
2024-08-03 22:29:14 +0900

    Add files via upload

--- 13_4.py
+++ 13_4.py
@@ -14,23 +14,33 @@ class UserState(StatesGroup):
     weight = State()
 
 
+@dp.message_handler(text='Привет')
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью. Введите слово Calories')
+
+
 @dp.message_handler(text=['Calories'])
 async def set_age(message):
-    print('Введите свой возраст:')
+    await message.answer('Введите свой возраст:')
     await UserState.age.set()
 
 
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
     await state.update_data(first=message.text)
-    print('Введите свой рост:')
+    await message.answer('Введите свой рост:')
     await UserState.growth.set()
 
 
 @dp.message_handler(state=UserState.growth)
 async def set_weight(message, state):
     await state.update_data(second=message.text)
-    print('Введите свой вес:')
+    await message.answer('Введите свой вес:')
     await UserState.weight.set()
 
 
@@ -38,8 +48,8 @@ async def set_weight(message, state):
 async def send_calories(message, state):
     await state.update_data(third=message.text)
     data = await state.get_data()
-    podschet_kallorii = 10*data['third']+6.25*data['second']+5*data['first']+5
-    await message.answer(f'Ваша норма каллори составляет: {podschet_kallorii}')
+    podschet_kallorii = (10*data['third'])+(6.25*data['second'])-(5*data['first'])+5
+    await message.answer(f'Ваша суточная норма каллорий составляет: {podschet_kallorii}')
     await state.finish()
 
 
